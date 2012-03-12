41693101c90b53d56ef7096965ded3313af080de
Rovin Bhandari <rovinbhandari@gmail.com>
2012-03-12 10:57:35 +0530

    minor changes

--- client_ftp.h
+++ client_ftp.h
@@ -18,6 +18,7 @@ struct command* userinputtocommand(char [LENUSERINPUT]);
 void printcommand(struct command*);
 
 void command_pwd(struct packet*, struct packet*, int);
+void command_lcd(char*);
 void command_cd(struct packet*, struct packet*, int, char*);
 void command_lls(char*);
 void command_ls(struct packet*, struct packet*, int);
@@ -27,8 +28,7 @@ void command_mget(struct packet*, struct packet*, int, int, char**);
 void command_mput(struct packet*, struct packet*, int, int, char**);
 void command_mgetwild(struct packet*, struct packet*, int);
 void command_mputwild(struct packet*, struct packet*, int, char*);
-void command_rput(struct packet*, struct packet*, int);
-void command_mkdir(struct packet*, struct packet*, int, char*);
 void command_lmkdir(char*);
-void command_lcd(char*);
+void command_mkdir(struct packet*, struct packet*, int, char*);
+void command_rput(struct packet*, struct packet*, int);
 
